f9d5f433884cc0a168c5726f077badfcb6f8ca9f
Tran Cong Viet An <antcv@dashsolutions.com>
2025-01-08 02:57:21 +0700

    feat: add fuzzy index for metal

--- src/main.rs
+++ src/main.rs
@@ -12,39 +12,38 @@ mod ray;
 mod utils;
 mod vec3;
 
-const MAT_GROUND: Lambertian = Lambertian::new(&Color::val(0.8, 0.8, 0.0));
-const MAT_CENTER: Lambertian = Lambertian::new(&Color::val(0.1, 0.2, 0.5));
-const MAT_LEFT: Metal = Metal::new(&Color::val(0.8, 0.8, 0.8));
-const MAT_RIGHT: Metal = Metal::new(&Color::val(0.8, 0.6, 0.2));
-
 fn main() {
     let mut world = HittableList::empty();
+
+    let mat_ground = Lambertian::new(&Color::val(0.8, 0.8, 0.0));
+    let mat_center = Lambertian::new(&Color::val(0.1, 0.2, 0.5));
+    let mat_left = Metal::new(&Color::val(0.8, 0.8, 0.8), 0.3);
+    let mat_right = Metal::new(&Color::val(0.8, 0.6, 0.2), 1.0);
 
     world.add(Rc::new(Sphere {
         center: Point::val(0.0, -100.5, -1.0),
         radius: 100.0,
-        material: Rc::new(MAT_GROUND),
+        material: Rc::new(mat_ground),
     }));
     world.add(Rc::new(Sphere {
         center: Point::val(0.0, 0.0, -1.2),
         radius: 0.5,
-        material: Rc::new(MAT_CENTER),
+        material: Rc::new(mat_center),
     }));
     world.add(Rc::new(Sphere {
         center: Point::val(-1.0, 0.0, -1.0),
         radius: 0.5,
-        material: Rc::new(MAT_LEFT),
+        material: Rc::new(mat_left),
     }));
     world.add(Rc::new(Sphere {
         center: Point::val(1.0, 0.0, -1.0),
         radius: 0.5,
-        material: Rc::new(MAT_RIGHT),
+        material: Rc::new(mat_right),
     }));
 
-
     let camera = Camera::new(CameraOption {
-        image_width: 400,
-        image_height: (400.0 / (16.0 / 9.0)) as i32,
+        image_width: 1000,
+        image_height: (1000.0 / (16.0 / 9.0)) as i32,
         viewport_height: 2.0,
         position: Point::val(0.0, 0.0, 0.0),
         focal_length: 1.0,
--- src/material/metal.rs
+++ src/material/metal.rs
@@ -1,20 +1,34 @@
-use crate::{object::HitRecord, ray::Ray, vec3::Color};
+use crate::{
+    object::HitRecord,
+    ray::Ray,
+    vec3::{Color, Vec3},
+};
 
 use super::{Material, ScatterResult};
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub const fn new(albedo: &Color) -> Self {
-        Self { albedo: *albedo }
+    pub fn new(albedo: &Color, fuzz: f64) -> Self {
+        Self {
+            albedo: *albedo,
+            fuzz: f64::min(f64::max(fuzz, 0.0), 1.0),
+        }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, r_in: &Ray, hit_record: &HitRecord) -> Option<ScatterResult> {
-        let reflected_dir = r_in.dir.reflect(&hit_record.normal);
+        let mut reflected_dir = r_in.dir.reflect(&hit_record.normal);
+        if self.fuzz > 0.0 {
+            reflected_dir = reflected_dir.unit() + self.fuzz * Vec3::rand_unit();
+        }
+        if reflected_dir.dot(&hit_record.normal) <= 0.0 {
+            return None;
+        }
         let scattered_ray = Ray {
             dir: reflected_dir,
             origin: hit_record.point,
